f70d791a27f240e91397711dd4375cd63b0452c8
ainozaki <aiai10piano@gmail.com>
2024-05-26 22:41:37 +0900

    Copy from mlir-aie

---
+++ setup_env.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/tools/Xilinx/Vitis/2023.2/aietools/bin:/tools/Xilinx/Vitis/2023.2/bin
+export XILINXD_LICENSE_FILE=/home/ainno/tools/Xilinx.lic
+source ~/source/mlir-aie/ironenv/bin/activate
+source ~/source/mlir-aie/utils/env_setup.sh ~/source/mlir-aie/my_install/mlir_aie ~/source/mlir-aie/my_install/mlir
---
+++ setup_net.sh
@@ -0,0 +1,2 @@
+ip link add vmnic0 type dummy
+ip link set vmnic0 addr 00:15:5d:b8:89:2b
