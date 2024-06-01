e6e8b5dfbe8788fe095faa20ca4f18c6644c23e6
ainozaki <aiai10piano@gmail.com>
2024-06-01 14:24:55 +0900

    2 columns

--- ntt/aie2.py
+++ ntt/aie2.py
@@ -28,7 +28,7 @@ def my_vector_add():
         raise ValueError("[ERROR] Need 2 command line arguments (Device name, Col)")
 
     if sys.argv[1] == "npu":
-        dev = AIEDevice.npu1_1col
+        dev = AIEDevice.npu1_4col
     elif sys.argv[1] == "xcvc1902":
         dev = AIEDevice.xcvc1902
     else:
@@ -44,42 +44,53 @@ def my_vector_add():
         _0_ShimTile = tile(0, 0)
         _0_ComputeTile2 = tile(0, 2)
         
-        #_1_ShimTile = tile(1, 0)
-        #_1_ComputeTile2 = tile(1, 2)
+        _1_ShimTile = tile(1, 0)
+        _1_ComputeTile2 = tile(1, 2)
         
+        ComputeTiles = [_0_ComputeTile2, _1_ComputeTile2]
         
         # AIE-array data movement with object fifos
         of_in0 = object_fifo("in0", _0_ShimTile, _0_ComputeTile2, buffer_depth, memRef_ty)
-        of_out = object_fifo("out", _0_ComputeTile2, _0_ShimTile, buffer_depth, memRef_ty)
+        of_in1 = object_fifo("in1", _1_ShimTile, _1_ComputeTile2, buffer_depth, memRef_ty)
+        of_out0 = object_fifo("out0", _0_ComputeTile2, _0_ShimTile, buffer_depth, memRef_ty)
+        of_out1 = object_fifo("out1", _1_ComputeTile2, _1_ShimTile, buffer_depth, memRef_ty)
+        of_ins = [of_in0, of_in1]
+        of_outs = [of_out0, of_out1]
 
         # Set up compute tiles
 
         # Compute tile 2
-        @core(_0_ComputeTile2)
-        def core_body():
-            # Effective while(1)
-            for _ in for_(sys.maxsize):
-                # Number of sub-vector "tile" iterations
-                for _ in for_(N_div_n):
-                    elem_in0 = of_in0.acquire(ObjectFifoPort.Consume, 1)
-                    elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
-                    for i in for_(n):
-                        v0 = memref.load(elem_in0, [i])
-                        v2 = arith.addi(v0, v0)
-                        memref.store(v2, elem_out, [i])
+        for ct in range(0, 2):
+            @core(ComputeTiles[ct])
+            def core_body():
+                # Effective while(1)
+                for _ in for_(sys.maxsize):
+                    # Number of sub-vector "tile" iterations
+                    for _ in for_(N_div_n):
+                        elem_in0 = of_ins[ct].acquire(ObjectFifoPort.Consume, 1)
+                        elem_out = of_outs[ct].acquire(ObjectFifoPort.Produce, 1)
+                        for i in for_(n):
+                            v0 = memref.load(elem_in0, [i])
+                            if ct == 0:
+                                v2 = arith.addi(v0, v0)
+                            else:
+                                v2 = arith.muli(v0, v0)
+                            memref.store(v2, elem_out, [i])
+                            yield_([])
+                        of_ins[ct].release(ObjectFifoPort.Consume, 1)
+                        of_outs[ct].release(ObjectFifoPort.Produce, 1)
                         yield_([])
-                    of_in0.release(ObjectFifoPort.Consume, 1)
-                    of_out.release(ObjectFifoPort.Produce, 1)
                     yield_([])
-                yield_([])
 
         # To/from AIE-array data movement
         tensor_ty = T.memref(N, T.i32())
 
         @FuncOp.from_py_func(tensor_ty, tensor_ty)
         def sequence(A, C):
-            npu_dma_memcpy_nd(metadata="out", bd_id=0, mem=C, sizes=[1, 1, 1, N])
+            npu_dma_memcpy_nd(metadata="out0", bd_id=0, mem=C, sizes=[1, 1, 1, N])
+            npu_dma_memcpy_nd(metadata="out1", bd_id=1, mem=C, sizes=[1, 1, 1, N])
             npu_dma_memcpy_nd(metadata="in0", bd_id=1, mem=A, sizes=[1, 1, 1, N])
+            npu_dma_memcpy_nd(metadata="in1", bd_id=2, mem=A, sizes=[1, 1, 1, N])
             npu_sync(column=0, row=0, direction=0, channel=0)
 
 
