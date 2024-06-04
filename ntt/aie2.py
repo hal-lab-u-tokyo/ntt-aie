55722b0d39464b5eeb8805f0d85a28f50d1f62fc
ainozaki <aiai10piano@gmail.com>
2024-06-04 16:51:29 +0900

    declare fifo with format

--- ntt/aie2.py
+++ ntt/aie2.py
@@ -77,40 +77,26 @@ def my_vector_add():
             object_fifo_link(of_ins_host[i], of_ins_core[i])
             object_fifo_link(of_outs_core[i], of_outs_host[i])
         
-        # lef-right
-        of_ct_02_03 = object_fifo("ct_02_03", ComputeTiles[0][0], ComputeTiles[0][1], buffer_depth, memRef_ty_core)
-        of_ct_03_04 = object_fifo("ct_03_04", ComputeTiles[0][1], ComputeTiles[0][2], buffer_depth, memRef_ty_core)
-        of_ct_04_05 = object_fifo("ct_04_05", ComputeTiles[0][2], ComputeTiles[0][3], buffer_depth, memRef_ty_core)
-        of_ct_12_13 = object_fifo("ct_12_13", ComputeTiles[1][0], ComputeTiles[1][1], buffer_depth, memRef_ty_core)
-        of_ct_13_14 = object_fifo("ct_13_14", ComputeTiles[1][1], ComputeTiles[1][2], buffer_depth, memRef_ty_core)
-        of_ct_14_15 = object_fifo("ct_14_15", ComputeTiles[1][2], ComputeTiles[1][3], buffer_depth, memRef_ty_core)
-        of_ct_22_23 = object_fifo("ct_22_23", ComputeTiles[2][0], ComputeTiles[2][1], buffer_depth, memRef_ty_core)
-        of_ct_23_24 = object_fifo("ct_23_24", ComputeTiles[2][1], ComputeTiles[2][2], buffer_depth, memRef_ty_core)
-        of_ct_24_25 = object_fifo("ct_24_25", ComputeTiles[2][2], ComputeTiles[2][3], buffer_depth, memRef_ty_core)
-        of_ct_32_33 = object_fifo("ct_32_33", ComputeTiles[3][0], ComputeTiles[3][1], buffer_depth, memRef_ty_core)
-        of_ct_33_34 = object_fifo("ct_33_34", ComputeTiles[3][1], ComputeTiles[3][2], buffer_depth, memRef_ty_core)
-        of_ct_34_35 = object_fifo("ct_34_35", ComputeTiles[3][2], ComputeTiles[3][3], buffer_depth, memRef_ty_core)
+        # lef-right (row)
+        ntt_left_right = []
+        ntt_right_left = []
+        for i in range(0, n_column):
+            ntt_left_right.append([])
+            ntt_right_left.append([])
+            for j in range(0, n_row - 1):
+                ntt_left_right[i].append(object_fifo(f"ct_{i}{j + 2}_{i}{j + 3}", ComputeTiles[i][j], ComputeTiles[i][j + 1], buffer_depth, memRef_ty_core))
+                ntt_right_left[i].append(object_fifo(f"ct_{i}{j + 3}_{i}{j + 2}", ComputeTiles[i][j + 1], ComputeTiles[i][j], buffer_depth, memRef_ty_core))
+        
         # top-bottom
-        of_ct_02_12 = object_fifo("ct_02_12", ComputeTiles[0][0], ComputeTiles[1][0], buffer_depth, memRef_ty_core)
-        of_ct_03_13 = object_fifo("ct_03_13", ComputeTiles[0][1], ComputeTiles[1][1], buffer_depth, memRef_ty_core)
-        of_ct_04_14 = object_fifo("ct_04_14", ComputeTiles[0][2], ComputeTiles[1][2], buffer_depth, memRef_ty_core)
-        of_ct_05_15 = object_fifo("ct_05_15", ComputeTiles[0][3], ComputeTiles[1][3], buffer_depth, memRef_ty_core)
-        of_ct_12_22 = object_fifo("ct_12_22", ComputeTiles[1][0], ComputeTiles[2][0], buffer_depth, memRef_ty_core)
-        of_ct_13_23 = object_fifo("ct_13_23", ComputeTiles[1][1], ComputeTiles[2][1], buffer_depth, memRef_ty_core)
-        of_ct_14_24 = object_fifo("ct_14_24", ComputeTiles[1][2], ComputeTiles[2][2], buffer_depth, memRef_ty_core)
-        of_ct_15_25 = object_fifo("ct_15_25", ComputeTiles[1][3], ComputeTiles[2][3], buffer_depth, memRef_ty_core)
-        of_ct_22_32 = object_fifo("ct_22_32", ComputeTiles[2][0], ComputeTiles[3][0], buffer_depth, memRef_ty_core)
-        of_ct_23_33 = object_fifo("ct_23_33", ComputeTiles[2][1], ComputeTiles[3][1], buffer_depth, memRef_ty_core)
-        of_ct_24_34 = object_fifo("ct_24_34", ComputeTiles[2][2], ComputeTiles[3][2], buffer_depth, memRef_ty_core)
-        of_ct_25_35 = object_fifo("ct_25_35", ComputeTiles[2][3], ComputeTiles[3][3], buffer_depth, memRef_ty_core)
-        ntt_left_right = [of_ct_02_03, of_ct_04_05, of_ct_12_13, of_ct_14_15, of_ct_22_23, of_ct_24_25, of_ct_32_33, of_ct_34_35]
-        ntt_top_bottom = [of_ct_02_12, of_ct_03_13, of_ct_04_14, of_ct_05_15, of_ct_22_32, of_ct_23_33, of_ct_24_34, of_ct_25_35]
-        swap_left_right = [of_ct_03_04, of_ct_13_14, of_ct_23_24, of_ct_33_34]
-        swap_top_bottom = [of_ct_12_22, of_ct_13_23, of_ct_14_24, of_ct_15_25]
+        ntt_top_bottom = []
+        ntt_bottom_top = []
+        for i in range(0, n_column - 1):
+            ntt_top_bottom.append([])
+            ntt_bottom_top.append([])
+            for j in range(0, n_row):
+                ntt_top_bottom[i].append(object_fifo(f"ct_{i}{j + 2}_{i + 1}{j + 2}", ComputeTiles[i][j], ComputeTiles[i + 1][j], buffer_depth, memRef_ty_core))
+                ntt_bottom_top[i].append(object_fifo(f"ct_{i + 1}{j + 2}_{i}{j + 2}", ComputeTiles[i + 1][j], ComputeTiles[i][j], buffer_depth, memRef_ty_core))
             
-        # Buffer 
-        aComputeTile15 = Buffer(ComputeTiles[3][3], [16], T.i32(), "aComputeTile15") 
-
         # Compute tile 
         for column in range(0, n_column):
             for row in range(0, n_row):
@@ -122,7 +108,7 @@ def my_vector_add():
                         core_idx = column * n_row + row
                         if row % 2 == 0:
                             in_from_mem = of_ins_core[column][row].acquire(ObjectFifoPort.Consume, 1)
-                            out_to_right = ntt_left_right[column * 2 + row // 2].acquire(ObjectFifoPort.Produce, 1)
+                            out_to_right = ntt_left_right[column][row].acquire(ObjectFifoPort.Produce, 1)
                             out_to_mem = of_outs_core[column][row].acquire(ObjectFifoPort.Produce, 1)
                             for i in for_(N//n_array):
 	                              v0 = memref.load(in_from_mem, [i])
@@ -130,13 +116,13 @@ def my_vector_add():
 	                              memref.store(v0, out_to_right, [i])
 	                              memref.store(v0, out_to_mem, [i])
 	                              yield_([])
-                            ntt_left_right[column * 2 + row // 2].release(ObjectFifoPort.Produce, 1)
+                            ntt_left_right[column][row].release(ObjectFifoPort.Produce, 1)
                             of_ins_core[column][row].release(ObjectFifoPort.Consume, 1)
                             of_outs_core[column][row].release(ObjectFifoPort.Produce, 1)
                         else:
                             in_from_mem = of_ins_core[column][row].acquire(ObjectFifoPort.Consume, 1)
                             out_to_mem = of_outs_core[column][row].acquire(ObjectFifoPort.Produce, 1)
-                            in_from_left = ntt_left_right[column * 2 + row // 2].acquire(ObjectFifoPort.Consume, 1)
+                            in_from_left = ntt_left_right[column][row-1].acquire(ObjectFifoPort.Consume, 1)
                             for i in for_(N//n_array):
 	                              v0 = memref.load(in_from_mem, [i])
 	                              # NTT within a core
@@ -144,7 +130,7 @@ def my_vector_add():
 	                              v2 = arith.addi(v0, v1)
 	                              memref.store(v2, out_to_mem, [i])
 	                              yield_([])
-                            ntt_left_right[column * 2 + row // 2].release(ObjectFifoPort.Consume, 1)
+                            ntt_left_right[column][row-1].release(ObjectFifoPort.Consume, 1)
                             of_ins_core[column][row].release(ObjectFifoPort.Consume, 1)
                             of_outs_core[column][row].release(ObjectFifoPort.Produce, 1)
 
